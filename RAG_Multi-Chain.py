c7086c44cfa7cc0a5a5e577ecd1de41724a63b55
RabiEddin <ehdbs1028@naver.com>
2025-06-14 17:12:15 +0900

    llama3.1_basic_llm_ㅡMulti-Chain

--- RAG_Multi-Chain.py
+++ RAG_Multi-Chain.py
@@ -16,4 +16,7 @@ print(chain1.invoke({"korean_world": "한옥"}))
 
 chain2 = {"english_world": chain1} | prompt2 | llm | StrOutputParser()
 
-print(chain2.invoke({"korean_world": "한옥"}))
+print(chain2.invoke({"korean_world": "한옥"})) # invoke: llm으로부터 결과 반환받아서 한 번에 출력
+
+for chunk in chain2.stream({"korean_world": "한옥"}): # stream: llm으로부터 결과 반환받아서 실시간으로 출력.
+    print(chunk, end="", flush=True)
